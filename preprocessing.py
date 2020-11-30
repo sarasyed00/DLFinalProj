1b013245e659ada4df8a7b89377a4575484656ee
Micah <micahbruning1234@gmail.com>
2020-11-29 16:50:34 -0800

    added sentence tokenization

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,21 @@
 import json
+from nltk.tokenize import sent_tokenize
 
 def get_data(file_path):
     reviews = []
     ratings = []
     items = []
     print("Started Reading JSON file which contains multiple JSON items")
-    with open(file_path) as f:
+    with open(file_path, encoding="utf8") as f:
+        counter = 0
         for jsonObj in f:
+            counter += 1
             item = json.loads(jsonObj)
             items.append(item)
+
+            if counter is 100:
+                break
+
     for item in items:
         text = item["text"]
         # print(text)
@@ -17,12 +24,26 @@ def get_data(file_path):
         # print(stars)
         ratings.append(stars)
 
+
     return reviews, ratings
 
+def tokenize_reviews(reviews):
+    reviews_tokenized = []
+    for review in reviews:
+        reviews_tokenized.append(sent_tokenize(review))
+
+    return reviews_tokenized
 
 def main():
-    get_data('yelp_dataset/review.json')
-    return
+    reviews, ratings = get_data('./yelp_dataset/review.json')
+
+    # To get sent_tokenize to work, In python terminal: 
+    # >>> import nltk
+    # >>> nltk.download('punkt')
+
+    reviews_tokenized = structure_data(reviews)
+
+    #reviews, ratings = get_data('yelp_dataset/review.json')
 
 if __name__ == '__main__':
     main()
