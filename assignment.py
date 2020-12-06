77707681adedca835dc87212005d5e005b3fef30
Micah <micahbruning1234@gmail.com>
2020-12-06 11:44:13 -0600

    abby's updates

--- assignment.py
+++ assignment.py
@@ -2,63 +2,104 @@ import os
 import numpy as np
 import tensorflow as tf
 from preprocessing import *
+from sklearn.model_selection import train_test_split
+from keras.layers.embeddings import Embedding
+
 
 
 class Model(tf.keras.Model):
-    def __init__(self):
+    def __init__(self, vocab_size, embedding_matrix):
         super(Model, self).__init__()
 
         #Hyperparameters
-        self.batch_size = 1
+        self.batch_size = 5
         self.learning_rate = .01
-        self.hidden_size_1 = 100
+        self.hidden_size_1 = 1
         self.hidden_size_2 = 100
+        self.embedding_size = 100
+        self.review_words_length = 100
+        self.vocab_size = vocab_size
+        self.rnn_size = 100
+        self.num_classes = 2
+        self.E = embedding_matrix
 
         #Layers
         self.optimizer = tf.keras.optimizers.Adam(learning_rate = self.learning_rate)
         self.dense_1 = tf.keras.layers.Dense(self.hidden_size_1, activation='relu')
-        self.dense_2 = tf.keras.layers.Dense(self.hidden_size_2, activation='softmax')
+        self.dense_2 = tf.keras.layers.Dense(self.num_classes, activation='softmax')
+        self.lstm = tf.keras.layers.LSTM(self.rnn_size)
+        #self.conv_1 = tf.keras.layers.Conv1D(128, 5, activation = 'relu')
+        #self.max_pool = tf.keras.layers.MaxPool1D(pool_size=2,strides=1, padding='valid')
+        self.embedding_layer = Embedding(self.vocab_size, self.embedding_size, weights = [embedding_matrix], input_length=self.review_words_length,
+                                         trainable=True
 
-
-    def call(self, inputs):
-        layer1_output = self.dense_1(inputs)
-        layer2_ouput = self.dense_2(layer1_output)
-
-
-        return layer2_ouput
+    def call(self, inputs): #should output an array of batch_size x 2
+        embedding = self.embedding_layer(inputs)
+        #embedding = tf.nn.embedding_lookup(self.E, inputs)
+        #convoluted = self.conv_1(embedding)
+        #max_pooled = self.max_pool(convoluted)
+        lstm= self.lstm(embedding)
+        layer1_output = self.dense_1(lstm)
+        layer2_output = self.dense_2(layer1_output)
+        return layer2_output
 
     def loss(self, probabilities, ratings):
         return tf.math.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=ratings, logits=probabilities))
 
     def accuracy(self, probabilities, ratings):
-        pass
+        #print(probabilities)
+        #print(ratings)
+        accuracy = 0
+        for x in range(len(ratings)):
+            if tf.argmax(probabilities[x]) == ratings[x]:
+                accuracy+=1
+        return accuracy/len(ratings)
 
 def train(model, train_rewards, train_ratings):
     
     #TO DO: Shuffle Inputs
-    
     for i in range(0, len(train_rewards), model.batch_size):
         rewards_batch = train_rewards[i : i + model.batch_size]
         ratings_batch = train_ratings[i : i + model.batch_size]
-
         with tf.GradientTape() as tape:
             probabilities = model.call(rewards_batch)
-            loss = model.loss(probabilities, train_ratings)
-
+            ratings_batch = np.reshape(np.array(ratings_batch), [model.batch_size, 1])
+            loss = model.loss(probabilities, ratings_batch)
         gradients = tape.gradient(loss, model.trainable_variables)
         model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        print(loss)
+    #print(model.accuracy(probabilities, ratings_batch))
 
 
-    return None
+def test(model,test_reviews, test_ratings):
+    accuracies = []
+    for i in range(0, len(test_reviews), model.batch_size):
+        rewards_batch = test_reviews[i : i + model.batch_size]
+        ratings_batch = test_ratings[i : i + model.batch_size]
+        probabilities = model.call(rewards_batch)
+        ratings_batch = np.reshape(np.array(ratings_batch), [model.batch_size, 1])
+        loss = model.loss(probabilities, ratings_batch)
+        accuracy = model.accuracy(probabilities, ratings_batch)
+        accuracies.append(accuracy)
+    return tf.reduce_mean(accuracies)
 
-def test():
-    pass
+
 
 def main():
-    model = Model()
+    reviews, ratings = get_data('review.json')
+    rev_train, rev_test, rat_train, rat_test = train_test_split(reviews, ratings, test_size = .10, random_state=42)
+    rev_train, rev_test, vocab_size, tokenizer = tokenize_with_keras(rev_train,rev_test)
+    embed_matrix = embedding_matrix(vocab_size, tokenizer)
+    #print(embed_matrix.shape)
+    model = Model(vocab_size, embed_matrix)
+    train(model, rev_train,rat_train)
+    print(test(model, rev_test, rat_test))
+
+    #print(rev_test)
+    #print(vocab_size)
 
     #TO DO: Figure out split for train and test
-    train_rewards, train_ratings = get_data('./yelp_dataset/review.json')
+
     
 
 
--- preprocessing.py
+++ preprocessing.py
@@ -1,54 +1,91 @@
 import json
+import string
+import numpy as np
+
 from nltk.tokenize import sent_tokenize, word_tokenize
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 
 def get_data(file_path):
     reviews = []
     ratings = []
     items = []
+    x = 0
     print("Started Reading JSON file which contains multiple JSON items")
-    with open(file_path, encoding="utf8") as f:
+    with open(file_path, encoding="utf8") as f: #loading 100 reviews
         counter = 0
         for jsonObj in f:
             counter += 1
             item = json.loads(jsonObj)
             items.append(item)
-
-            if counter is 100:
+            if counter == 500:
                 break
-
     for item in items:
         text = item["text"]
-        # print(text)
+        text = clean_review(text) #cleans the review
         reviews.append(text)
         stars = item["stars"]
-        # print(stars)
         ratings.append(stars)
-
-
+    ratings = np.array(list(map(lambda x: 1 if x == 3.0 or x == 4.0 or x == 5.0 else 0, ratings))) #binary ratings
     return reviews, ratings
+
+def clean_review(review): #method that cleans a review by getting rid of punctuation and splitting on each word
+    text = review.split()  # tokenizing the text, split by words
+    table = str.maketrans('', '', string.punctuation)
+    text = [w.translate(table) for w in text]  # gets rid of the punctuation
+    # https://machinelearningmastery.com/develop-word-embedding-model-predicting-movie-review-sentiment/
+    text = [word for word in text if word.isalpha()]
+    text = ' '.join(text)
+    return text
+
+def tokenize_with_keras(training, testing):
+    tokenizer = Tokenizer(num_words = 5000) #look into what the propper num words is
+    tokenizer.fit_on_texts(training)
+    training = tokenizer.texts_to_sequences(training)
+    testing = tokenizer.texts_to_sequences(testing)
+    vocab_size = len(tokenizer.word_index) +1
+    length = 100 #we are cutting off our reviews at 100 words and padding them to be 100, we can change this later if we want
+    training = pad_sequences(training, padding='post', maxlen=length)
+    testing = pad_sequences(testing, padding='post', maxlen=length)
+    return training, testing, vocab_size, tokenizer
+
+def embedding_matrix(vocab_size, tokenizer):
+    embeddings_dictionary = dict()
+    glove_file = open('../glove.6B.100d.txt', encoding="utf8")
+    for line in glove_file:
+        records = line.split()
+        word = records[0]
+        vector_dimensions = np.asarray(records[1:], dtype='float32')
+        embeddings_dictionary[word] = vector_dimensions
+    glove_file.close()
+    embedding_matrix = np.zeros((vocab_size, 100))
+    for word, index in tokenizer.word_index.items():
+        embed_vector = embeddings_dictionary.get(word)
+        if embed_vector is not None:
+            embedding_matrix[index] = embed_vector
+    return embedding_matrix
+#https://stackabuse.com/python-for-nlp-movie-sentiment-analysis-using-deep-learning-in-keras/
 
 def get_tokenized_reviews(reviews, byWord=False):
     reviews_tokenized = []
-    
     for review in reviews:    
         #Tokenize by sentence or word based on bool
         if byWord:
             reviews_tokenized.append(word_tokenize(review))
         else:
             reviews_tokenized.append(sent_tokenize(review))
-
     return reviews_tokenized
 
-#def main():
-#    reviews, ratings = get_data('./yelp_dataset/review.json')
+def main():
+    reviews, ratings = get_data('review.json')
+    #tokenized_reviews = get_tokenized_reviews(reviews)
+    #print(reviews)
 
     # To get sent_tokenize to work, In python terminal: 
     # >>> import nltk
     # >>> nltk.download('punkt')
-
 #    reviews_tokenized = tokenize_reviews(reviews)
-
     #reviews, ratings = get_data('yelp_dataset/review.json')
 
-#if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    main()
